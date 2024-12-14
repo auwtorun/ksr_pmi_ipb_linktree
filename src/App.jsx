efe0fbf83b86bd375aeae32bc7ff8de9646623f9
muhputrardiansyah <muhputrardiansyah@gmail.com>
2024-12-14 08:52:30 +0000

    Fix Bug

--- src/App.jsx
+++ src/App.jsx
@@ -4,7 +4,7 @@ import Page from './pages/Page'
 const App = () => {
   return (
     <>
-    <div className='bg-[#FFEBD3] w-full h-screen'>
+    <div className='w-full h-screen'>
       <Page></Page>
     </div>
     </>
